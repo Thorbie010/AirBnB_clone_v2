9263f3824c7c7582234050bdd8b1afe62eb593b3
thorbie <jonahemmanuel54@gmail.com>
2023-10-20 14:49:11 +0100

    hbnb

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Script that starts a flask web application."""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """replace underscore with spaces in the text variables"""
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def display_python_is_cool(text):
+    """replace underscore with spaces in the text variables"""
+    text = text.replace('_', 'is cool')
+    return f"Python {text}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
